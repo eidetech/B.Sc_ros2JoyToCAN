788a8a39e77ec34b30d5bdd9809fbcc10b4edd55
eidetech <marcuseide@me.com>
2022-03-25 20:27:55 +0100

    Started working on simulink simulation of the robot

---
+++ matlab/simulink_simulation/params_malerobot.m
@@ -0,0 +1,29 @@
+clear; clc; close all;
+
+
+% World Frame er definert i (x,y) = (0,0).
+
+% Venstre side
+L1  = -0.5;
+L2 = -0.2;
+L3  = -0.5;
+L4 = 2;
+
+% Høyre side
+L5 = 2;
+L6 = -0.2;
+L7 = 2;
+L8 = 2;
+
+% Verktøy
+L9 = 0.1 % Tool X
+L10 = 0.1 % Tool Y
+
+
+% Radius og tykkelse på trinser
+R_t = 0.02
+T_t = 0.03
+
+% Radius og tykkelse på trommel
+R_d = 0.05
+T_d = 0.01
