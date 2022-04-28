04bb3b588c58739e821bd187092523b5f3758329
eidetech <marcuseide@me.com>
2022-04-29 00:26:00 +0200

    More work on the website and added default parameters as json file.

--- src/joy2can/scripts/app.py
+++ src/joy2can/scripts/app.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import json
 import os
+import git
+from git import repo
 
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import Float32MultiArray
-from std_msgs.msg import String
-from std_msgs.msg import Int8
 
 template_dir = '../../../../src/joy2can/scripts/templates'
 static_dir = '../../../../src/joy2can/scripts/templates/static'
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+app.config['JSON_SORT_KEYS'] = False
 
 class WebPublisher(Node):
 
@@ -27,7 +29,7 @@ class WebPublisher(Node):
         self.wall_height = 0
         self.x_offset = 0
         self.z_offset = 0
-        self.percent_overlap = 0
+        self.vertical_step = 0
 
         # 0 = stop_job
         # 1 = pause_job
@@ -43,7 +45,7 @@ class WebPublisher(Node):
                     float(self.wall_height),
                     float(self.x_offset),
                     float(self.z_offset),
-                    float(self.percent_overlap),
+                    float(self.vertical_step),
                     float(self.job_status)]
         self.publisher_.publish(msg)
 
@@ -58,7 +60,14 @@ if __name__ == '__main__':
     rclpy.spin_once(wp)
     @app.route("/")
     def home():
-        return render_template("index.html")
+        return render_template("index.html", job_status=0,
+                               frame_width=wp.frame_width,
+                               frame_height=wp.frame_height,
+                               wall_width=wp.wall_width,
+                               wall_height=wp.wall_height,
+                               x_offset=wp.x_offset,
+                               z_offset=wp.z_offset,
+                               vertical_step=wp.vertical_step)
 
 
     @app.route('/update_parameters', methods=['POST'])
@@ -70,7 +79,7 @@ if __name__ == '__main__':
             wp.wall_height = request.form['wall_height']
             wp.x_offset = request.form['x_offset']
             wp.z_offset = request.form['z_offset']
-            wp.percent_overlap = request.form['percent_overlap']
+            wp.vertical_step = request.form['vertical_step']
             rclpy.spin_once(wp)
             return render_template("index.html", job_status= 0,
                                                  frame_width=wp.frame_width,
@@ -79,30 +88,62 @@ if __name__ == '__main__':
                                                  wall_height=wp.wall_height,
                                                  x_offset=wp.x_offset,
                                                  z_offset=wp.z_offset,
-                                                 percent_overlap=wp.percent_overlap)
+                                                 vertical_step=wp.vertical_step)
 
     @app.route('/control')
     def control():
-            return render_template("control.html", frame_width=wp.frame_width)
+            return render_template("control.html", frame_width=float(wp.frame_width))
 
     @app.route('/start_job', methods=['POST', 'GET'])
     def start_job():
         wp.job_status = 2
         rclpy.spin_once(wp)
-        return render_template("control.html", job_status=wp.job_status)
+        return render_template("control.html", job_status=wp.job_status, frame_width=float(wp.frame_width))
 
     @app.route('/pause_job', methods=['POST', 'GET'])
     def pause_job():
         wp.job_status = 1
         rclpy.spin_once(wp)
-        return render_template("control.html", job_status=wp.job_status)
+        return render_template("control.html", job_status=wp.job_status,  frame_width=float(wp.frame_width))
 
     @app.route('/stop_job', methods=['POST', 'GET'])
     def stop_job():
         wp.job_status = 0
         rclpy.spin_once(wp)
-        return render_template("control.html", job_status=wp.job_status)
+        return render_template("control.html", job_status=wp.job_status, frame_width=float(wp.frame_width))
 
+    @app.route('/load_default_params', methods=['POST', 'GET'])
+    def load_default_params():
+        default_params_path = os.path.join(os.path.dirname(__file__), "default.json")
+        with open(default_params_path) as f:
+            data = json.load(f)
+
+        wp.frame_width = data['default']['frame_width']
+        wp.frame_height = data['default']['frame_height']
+        wp.wall_width = data['default']['wall_width']
+        wp.wall_height = data['default']['wall_height']
+        wp.x_offset = data['default']['x_offset']
+        wp.z_offset = data['default']['z_offset']
+        wp.vertical_step = data['default']['vertical_step']
+        wp.job_status = data['default']['job_status']
+        print("wp frame:", data['default']['frame_width'])
+        rclpy.spin_once(wp)
+        return render_template("index.html", job_status=float(wp.job_status),
+                               frame_width=float(wp.frame_width),
+                               frame_height=float(wp.frame_height),
+                               wall_width=float(wp.wall_width),
+                               wall_height=float(wp.wall_height),
+                               x_offset=float(wp.x_offset),
+                               z_offset=float(wp.z_offset),
+                               vertical_step=float(wp.vertical_step))
+
+    @app.route('/git_pull',  methods=['POST'])
+    def git_pull():
+        g = git.cmd.Git('/home/marcus/github/B.Sc_ros2JoyToCAN')
+        g.pull()
+        repo = git.Repo(search_parent_directories=True)
+        hash = repo.head.object.hexsha
+        return render_template("index.html", git_pull_done=True, hash=hash[:7])
 
     app.debug = True
     app.config['EXPLAIN_TEMPLATE_LOADING'] = True
